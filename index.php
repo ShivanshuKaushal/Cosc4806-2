fe166f8c14961a05b28d24446dbafd69bed1a81b
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-06-01 02:14:52 +0000

    added user page

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once('database.php');
+require_once('user.php');
+if (!isset($_SESSION['authenticated'])) {
+  header("Location: login.php");
+}
+
+require_once('user.php');
+
+$user = new User();
+$user_list = $user->get_all_users();
+
+echo "<pre>";
+print_r($user_list);
+
+?>
+
+<html>
+  <head>
+    <title>Shivanshu </title>
+  </head>
+
+  <body>
+    <h1>Assignment 2</h1>
+    <p> Welcome, <?=$_SESSION['username'] ?></p> 
+    <?php echo "Today is " . date("l, F j, Y") . "."?>
+
+  </body>
+
+  <footer> <a href="/logout.php">Click here to Logout</a> </footer>
+</html>
---
+++ user.php
@@ -0,0 +1,30 @@
+<?php
+require_once('database.php');
+
+class User {
+
+    public function get_all_users() {
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM users");
+        $statement->execute();
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+   ]
+    public function isUser_exists($username) {
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM users WHERE username = :username");
+        $statement->bindParam(':username', $username);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function register_user($username, $password) {
+        $db = db_connect();
+        $statement = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+        $statement->bindParam(':username', $username);
+        $statement->bindParam(':password', $password);
+        $statement->execute();
+    }
+}
+?>
