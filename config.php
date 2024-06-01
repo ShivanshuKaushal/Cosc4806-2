d149100b53be5e498af1c843d4d3fe05ed9c6ff9
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-06-01 02:02:23 +0000

    Created login page

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST',     'm43.h.filess.io');
+define('DB_USER',     'Cosc4806_rhythmkept');
+define('DB_PASS',     $_ENV['DB_PASS']);
+define('DB_DATABASE', 'Cosc4806_rhythmkept');
+define('DB_PORT',     '3305');
+
+?>
